72c94a681644513d0ea5daa73a5a83595191358b
asters1 <gj744559140@163.com>
2023-12-18 14:25:33 +0000

    html

--- html/js/index.js
+++ html/js/index.js
@@ -1,5 +1,7 @@
 window.onload = function () {
+  var filter = {}
   var homeContent = document.getElementById('homeContent')
+  var homeContent_filter = document.getElementById('homeContent_filter')
 
   var xhr = new XMLHttpRequest()
   xhr.open(
@@ -17,15 +19,54 @@ window.onload = function () {
       }
       var home = document.createElement('span')
       home.innerText = '主页'
-      home.setAttribute('class', 'home title')
+      home.setAttribute('class', 'home type_name')
+      home.setAttribute('id', 'home')
       homeContent.appendChild(home)
       var classes = res['data']['class']
       for (var i = 0; i < classes.length; i++) {
-        console.log(classes[i])
+        var span = document.createElement('span')
+        span.innerText = classes[i]['type_name']
+        span.setAttribute('class', 'type_name')
+        span.setAttribute('id', classes[i]['type_id'])
+        span.setAttribute('tid', classes[i]['type_id'])
+        span.setAttribute('tname', classes[i]['type_name'])
+        span.setAttribute(
+          'onclick',
+          'type_name_onclick("' + classes[i]['type_id'] + '")',
+        )
+        homeContent.appendChild(span)
       }
+      filter = res['data']['filter']
     } else {
       console.log('获取homeContent参数失败!!!')
     }
   }
   xhr.send()
+  //加载函数
+  type_name_onclick = function (click_id) {
+    var flr = filter[click_id]
+    for (i = 0; i < flr.length; i++) {
+      k = flr[i]['key']
+      n = flr[i]['name']
+      v = flr[i]['value']
+      // console.log(k)
+      var span = document.createElement('span')
+      var div_null = document.createElement('div')
+      span.setAttribute('id', k)
+      span.setAttribute('key', k)
+      span.setAttribute('class', 'filter_name')
+      span.innerText = n
+      div_null.appendChild(span)
+      for (j = 0; j < v.length; j++) {
+        // console.log(v)
+        var span = document.createElement('span')
+        span.innerText = v[j]['n']
+        span.setAttribute('class', 'filter_value')
+
+        div_null.appendChild(span)
+      }
+
+      homeContent_filter.appendChild(div_null)
+    }
+  }
 }
