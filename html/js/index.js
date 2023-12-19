71b03d4ca716495e64ee594a3639e1d4482376d0
asters1 <gj744559140@163.com>
2023-12-19 05:33:20 +0000

    html

--- html/js/index.js
+++ html/js/index.js
@@ -44,10 +44,22 @@ window.onload = function () {
   xhr.send()
   //加载函数
   type_name_onclick = function (click_id) {
+    for (var i = 1; i < homeContent.children.length; i++) {
+      var child = homeContent.children[i]
+      // console.log(child) // 输出子元素
+      child.setAttribute('class', 'type_name')
+    }
+    element_click_id = document.getElementById(click_id)
+    element_click_id.setAttribute('class', 'check_type_name')
+
     while (homeContent_filter.firstChild) {
       homeContent_filter.removeChild(homeContent_filter.firstChild)
     }
     var flr = filter[click_id]
+    if (typeof flr === 'undefined') {
+      return
+    }
+
     for (i = 0; i < flr.length; i++) {
       k = flr[i]['key']
       n = flr[i]['name']
@@ -57,6 +69,7 @@ window.onload = function () {
       var div_null = document.createElement('div')
       div_null.setAttribute('filter_key', k)
       div_null.setAttribute('filter_name', n)
+      div_null.setAttribute('id', 'div_' + k)
       span.setAttribute('id', k)
       span.setAttribute('key', k)
       span.setAttribute('class', 'filter_name')
@@ -67,13 +80,34 @@ window.onload = function () {
         var span = document.createElement('span')
         span.innerText = v[j]['n']
         span.setAttribute('class', 'filter_value')
-
+        span.setAttribute('id', k + '$$$$$' + v[j]['v'])
+        // span.setAttribute('id', 'a$' + j)
+        // span.setAttribute('v', v[j]['v'])
         div_null.appendChild(span)
+        span.setAttribute(
+          'onclick',
+          'filter_onclick("div_' + k + '","' + k + '$$$$$' + v[j]['v'] + '")',
+        )
       }
 
       homeContent_filter.appendChild(div_null)
     }
     var hr = document.createElement('hr')
     homeContent_filter.appendChild(hr)
+  }
+  filter_onclick = function (parentId, fid) {
+    // alert(parentId + ':' + value)
+    element_pId = document.getElementById(parentId)
+    element_click_filter_id = document.getElementById(fid)
+
+    console.log(element_click_filter_id)
+
+    for (var i = 1; i < element_pId.children.length; i++) {
+      var child = element_pId.children[i]
+      // console.log(child) // 输出子元素
+      child.setAttribute('class', 'filter_value')
+    }
+
+    element_click_filter_id.setAttribute('class', 'check_filter_value')
   }
 }
